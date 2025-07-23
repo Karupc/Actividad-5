5f221fcb168428de9d6f6449ab12f1306733facd
Karup <kperca802@gmail.com>
2025-07-22 19:16:08 -0600

    Menú arreglado

--- Actividad 5.py
+++ Actividad 5.py
@@ -1,11 +1,11 @@
+ventas = []
 while True:
-print("\n--MENÚ DE ANÁLISIS DE VENTA--")
-opciones = input(f"1. Ingresar lista de ventas (valores enteros positivos)\n2. Mostrar todas las ventas ingresadas\n3. Calcular la venta más alta y la más baja\n4. Calcular promedio de ventas\n5. Contar cuántos días superaron los Q1000\n6. Buscar si una venta específica existe en la lista\n7. Clasificar cada venta: alta (>1000), media (500–1000), baja (<500)\n8. Salir\nIngrese el número de la opción que desea seleccionar: ")
-match opciones:
-    case "1":
-        ventas = int(input("Ingrese el número de ventas que desea agregar: "))
-        ventass = []
-        for i in range(ventas):
-            producto = input("Ingrese el producto")
-            venta = int(input("Ingrese eL valor de la venta en enteros positivos: Q"))
-            ventass.appent(producto, venta)
+    print("MENÚ DE ANÁLISIS DE VENTAS")
+    print("1. Ingresar ventas")
+    print("2. Mostrar ventas")
+    print("3. Venta más alta y más baja")
+    print("4. Promedio de ventas")
+    print("5. Días con ventas > Q1000")
+    print("6. Clasificar ventas")
+    print("7. Salir")
+    opcion = input("Ingresa el número de la opción que deseas seleccionar: ")
